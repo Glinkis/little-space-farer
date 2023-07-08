744ea35f635dc2da97dcef88bb7fe246a8120c0d
Victor Glindås <glinkis@gmail.com>
2023-07-08 14:47:26 +0200

    update camera controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,13 +2,45 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Camera cameraObject;
+    Camera cameraObject;
+    Transform center;
+    Transform target;
 
-    public GameObject center;
+    void Awake()
+    {
+        cameraObject = GetComponentInChildren<Camera>();
+        center = GetComponent<Transform>();
+        target = center;
+    }
 
     void Update()
     {
+        SelectTarget();
+        CenterTarget();
         RotateWithMouse();
+    }
+
+    void SelectTarget()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit, Camera.main.farClipPlane))
+                if (raycastHit.transform.gameObject)
+                    target = raycastHit.transform;
+        }
+    }
+
+    void CenterTarget()
+    {
+        center.transform.position = Vector3.Slerp(
+            center.position,
+            target.position,
+            Time.deltaTime * 10
+        );
+
+        cameraObject.transform.LookAt(center.position);
     }
 
     void RotateWithMouse()
@@ -16,13 +48,13 @@ public class CameraController : MonoBehaviour
         if (Input.GetMouseButton(1))
         {
             cameraObject.transform.RotateAround(
-                center.transform.position,
+                center.position,
                 cameraObject.transform.up,
                 Input.GetAxis("Mouse X") * 10
             );
 
             cameraObject.transform.RotateAround(
-                center.transform.position,
+                center.position,
                 cameraObject.transform.right,
                 Input.GetAxis("Mouse Y") * -10
             );
